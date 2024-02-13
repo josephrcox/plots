dc5bb14946d15edde49e4f8544217b10e2354dc5
Joseph Cox <joeyhobart@outlook.com>
2024-02-12 22:17:20 -0500

    build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/plots/",
-  plugins: [svelte()]
-})
+	plugins: [svelte()],
+	build: {
+		outDir: './dist', // relative to index.html
+		// emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
+	},
+});
