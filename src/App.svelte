bc305f923794b248f4f6ca522435b3567f64e0ee
Joseph Cox <joeyhobart@outlook.com>
2024-03-08 21:49:14 -0500

    Adding annoying error to stop problems until app is responsive

--- src/App.svelte
+++ src/App.svelte
@@ -32,6 +32,15 @@
 
 	let isOnReferencePlot = false;
 
+	// if screen width drops below 800, show an alert
+	window.addEventListener('resize', () => {
+		if (window.innerWidth < 800) {
+			alert(
+				'Your screen is too small to play this game. Please use a larger screen.',
+			);
+		}
+	});
+
 	// if key P is pressed, pause the game
 	document.addEventListener('keydown', (e) => {
 		// if input then return
