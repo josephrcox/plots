fc0708e89abe3ea908736f7d3cf017b0378679f9
Joseph Cox <joeyhobart@outlook.com>
2024-12-19 12:59:39 -0500

    build adjustments

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import path from 'path';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 export default defineConfig({
-	base: '/plots/',
-	plugins: [svelte()],
-	build: { outDir: './docs', assetsDir: 'assets', watch: true },
-	resolve: { alias: { $lib: path.resolve('./src/lib') } },
+  base: "/",
+  plugins: [svelte()],
+  build: { outDir: "./docs", assetsDir: "assets", watch: true },
+  resolve: { alias: { $lib: path.resolve("./src/lib") } },
 });
